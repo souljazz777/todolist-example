b60733888138f06a75dcb63a114ba0b6586c685b
souljazz777 <tyemid0117@gmail.com>
2022-03-25 14:38:51 +0800

    設定環境

---
+++ server.js
@@ -0,0 +1,111 @@
+const { lstat } = require('fs');
+const http = require('http')
+const { v4: uuidv4 } = require('uuid');
+const errorHandel = require('./errorHandel')
+const todos = [];
+
+const requestListener = (req,res)=>{
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json'
+    }
+    let body = "";
+    let num = 0;
+    //get http request body data
+    req.on('data', chunk=>{
+        body += chunk;
+    })
+    // console.log(req)
+    // console.log(req.method)
+    if (req.url=='/todos' && req.method == "GET"){
+        res.writeHead(200,headers)
+        res.write(JSON.stringify({
+            "status": "success",
+            "data": todos,
+        }))
+        res.end()
+    } else if(req.url=='/todos' && req.method == "POST"){
+        req.on('end', ()=>{
+            try{
+                const title = JSON.parse(body).title;
+                if(title !== undefined){
+                    const todo = {
+                        "title": title,
+                        "id": uuidv4()
+                    }
+                    todos.push(todo);
+                    res.writeHead(200,headers)
+                    res.write(JSON.stringify({
+                    "status": "success",
+                    "data": todos,
+                    }))
+                    res.end()
+                } else {
+                   errorHandel(res)
+                }
+            }catch(error){
+               errorHandel(res)
+            }            
+        })   
+    } else if(req.url=='/todos' && req.method == "DELETE"){
+        todos.length = 0; //清除todos裡面存放的值
+        res.writeHead(200,headers)
+        res.write(JSON.stringify({
+            "status": "success",
+            "data": todos,
+        }))
+        res.end()
+    }else if (req.url.startsWith("/todos/") && req.method == "DELETE"){
+        const id = req.url.split('/').pop();
+        const index = todos.findIndex(element => element.id == id)
+        if(index !== -1){
+            todos.splice(index,1)
+            res.writeHead(200,headers)
+            res.write(JSON.stringify({
+            "status": "success",
+            "data": todos,
+        }))
+        res.end()
+        } else {
+            errorHandel(res)
+        }
+        
+    }else if(req.url.startsWith("/todos/") && req.method == "PATCH"){
+        req.on('end',()=>{
+            try{
+                const todo = JSON.parse(body).title;
+                const id = req.url.split('/').pop();
+                const index = todos.findIndex(element => element.id == id) // index取得索引值
+                // console.log(index)
+                if(todo !== undefined && index !== -1){
+                    todos[index].title = todo;
+                     res.writeHead(200,headers)
+                     res.write(JSON.stringify({
+                         "status": "success",
+                         "data": todos,
+                     }))
+                     res.end();
+                } else {
+                    errorHandel(res);
+                }
+            }catch{
+                errorHandel(res);
+            }
+        })
+    }else if (req.method == "OPTIONS"){
+        res.writeHead(200,headers)
+        res.end()
+    } else {
+        res.writeHead(404,headers)
+        res.write(JSON.stringify({
+            "status": "false",
+            "message": "無此網站路由",
+        }))
+        res.end()
+    }
+    
+}
+const server = http.createServer(requestListener);
+server.listen(process.env.PORT || 3000);
